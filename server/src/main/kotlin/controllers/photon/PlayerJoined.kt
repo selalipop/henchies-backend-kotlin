4c9d67125810800add5dcdb84401c2ff94564071
Selali Adobor <selali.adobor@gmail.com>
2020-11-14 16:25:03 -0500

    Add early return when player is already in game

--- server/src/main/kotlin/controllers/photon/PlayerJoined.kt
+++ server/src/main/kotlin/controllers/photon/PlayerJoined.kt
@@ -1,6 +1,5 @@
 package controllers.photon
 
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import models.GamePhase
 import models.GameState
@@ -12,31 +11,31 @@ import repository.GameStateStore
 import util.logger
 
 
-suspend fun processPlayerJoined(gameId: GameId, playerId: PlayerId, gameStateStore: GameStateStore) =
-    coroutineScope {
-        gameStateStore.updateGameState(gameId) { oldState ->
-            if (oldState.players.any { it.id == playerId }) {
-                logger.info { "received player joined event but player was already in game, player:$playerId game: $gameId " }
-            }
-
-            val unusedColor = PlayerColor.SelectableColors.shuffled().first { color ->
-                oldState.players.none { it.color == color }
-            }
-
-            val newState = oldState.copy(players = oldState.players + PlayerState(playerId, unusedColor))
-
-            if (newState.players.size < oldState.maxPlayers) {
-                return@updateGameState newState
-            }
-
-            if (newState.players.size > oldState.maxPlayers) {
-                logger.warn { "exceeded max player count for game before starting for game $gameId" }
-            }
-
-            logger.info { "starting game $gameId after player $playerId joined" }
-            return@updateGameState startGame(newState)
+suspend fun processPlayerJoined(gameId: GameId, playerId: PlayerId, gameStateStore: GameStateStore) {
+    gameStateStore.updateGameState(gameId) { oldState ->
+        if (oldState.players.any { it.id == playerId }) {
+            logger.info { "received player joined event but player was already in game, player:$playerId game: $gameId " }
+            return@updateGameState oldState
         }
+
+        val unusedColor = PlayerColor.SelectableColors.shuffled().first { color ->
+            oldState.players.none { it.color == color }
+        }
+
+        val newState = oldState.copy(players = oldState.players + PlayerState(playerId, unusedColor))
+
+        if (newState.players.size < oldState.maxPlayers) {
+            return@updateGameState newState
+        }
+
+        if (newState.players.size > oldState.maxPlayers) {
+            logger.warn { "exceeded max player count for game before starting for game $gameId" }
+        }
+
+        logger.info { "starting game $gameId after player $playerId joined" }
+        return@updateGameState startGame(newState)
     }
+}
 
 
 private suspend fun startGame(state: GameState): GameState {
