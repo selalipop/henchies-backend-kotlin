f16a8dd96b3967a4a7389cef96643af3c8941334
Selali Adobor <selali.adobor@gmail.com>
2021-04-30 12:26:29 -0400

    Add additional logging for player left event

--- server/src/main/kotlin/controllers/photon/PlayerLeftController.kt
+++ server/src/main/kotlin/controllers/photon/PlayerLeftController.kt
@@ -2,11 +2,11 @@ package controllers.photon
 
 import io.ktor.application.*
 import io.ktor.request.*
-import io.ktor.response.*
 import models.byId
 import repository.GameStateStore
 import schema.requests.photon.OkReply
 import schema.requests.photon.PlayerLeftRequest
+import util.logger
 
 
 class PlayerLeftController(
@@ -15,12 +15,17 @@ class PlayerLeftController(
 
     suspend fun playerLeft(ctx: ApplicationCall) {
         val request = ctx.receive<PlayerLeftRequest>()
-
+        logger.info { "Processing player ${request.playerId} leaving game ${request.gameId} " }
         gameStateStore.updateGameState(request.gameId) { state ->
             val gonePlayer = state.players.byId(request.playerId)
 
             if (!state.isGameStarted && gonePlayer != null) {
                 return@updateGameState state.copy(players = state.players - gonePlayer)
+            }
+
+            logger.info {
+                "Ignoring player ${request.playerId} leaving game ${request.gameId}." +
+                        " Game is started? ${state.isGameStarted} Player is missing? ${gonePlayer == null} Players Left ${state.players.joinToString { "," }}"
             }
 
             return@updateGameState state
